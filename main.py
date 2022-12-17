fbfe6e2e6bf7c38d61d3a9303865ce9b847cfd2f
Ram Patel <>
2022-12-17 15:23:45 +0000

    Initial commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import translators as ts
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+
 desc_split = '----------------------------------------------------------------'
 def loadLangs():
   # Reading in compat.txt as a file
-  with open("compat.txt") as f:
+  with open("compat.txt", "r") as f:
     # trim delimiters and split each line into language and language mapping
     lines = [line.strip().split() for line in f.readlines()]
     m = {line[0]: line[1] for line in lines}
@@ -15,24 +16,25 @@ def loadLangs():
     # accounting for abbreviations
     for _, value in m.copy().items():
       m[value] = value
-    # return dictionary of language and language mapping
+
     return m
 
 lang_maps = loadLangs()
-# print(lang_maps)
+# References
 # print(ts.google(query_text="Hello", from_language=lang_maps['en'], to_language=lang_maps['mn']))
 
 app = Flask(__name__)
+# allows for CORS if front-end uses this API (needed)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
   # Load route names and their descriptions
   with open("routes.txt", "r") as routes:
-    # strip delimiters
+    # Trim delimiters
     paths = [route.strip() for route in routes.readlines()]
     with open("descriptions.txt", "r") as descs:
-      description_texts = descs.read().split('----------------------------------------------------------------')
+      description_texts = descs.read().split(desc_split)
     # Getting rid of routes with missing fields to make arrays zippable
     if len(paths) < len(description_texts):
       while len(description_texts) != len(paths):
@@ -42,7 +44,7 @@ def index():
         paths.pop()
     # strip delimiters and zip arrays together
     pairs = zip(paths, [text.strip() for text in description_texts])
-  # Returns JSON of documentation for this API.
+  # Return JSON of documentation for this API.
   return jsonify({"message": "Hello!", "currentRoutes": [{"route": r, "description": d} for r, d in pairs]})
 
 
@@ -55,8 +57,8 @@ def translate(from_lang, to_lang):
   json_data = request.get_json()
   # Check for whether 'text' field is within JSON data
   if 'text' not in json_data:
-    return jsonify({'error': 'Missing text JSON field!', 'mappings': lang_maps})
-  # Return translation as JSON
+    return jsonify({'error': 'No \'text\' field in JSON!', 'mappings': lang_maps})
+  
   return jsonify({'translation': ts.google(query_text=json_data['text'], from_language=lang_maps[from_lang], to_language=lang_maps[to_lang])})
 
 
@@ -69,14 +71,15 @@ def translate_default(to_lang):
   json_data = request.get_json()
   # Check if 'text' field is within JSON data.
   if 'text' not in json_data:
-    return jsonify({'error': 'Missing text JSON field!', 'mappings': lang_maps})
-  # Return translation as JSON
+    return jsonify({'error': 'No \'text\' field in JSON!', 'mappings': lang_maps})
+  
   return jsonify({'translation': ts.google(query_text=json_data['text'], to_language=lang_maps[to_lang])})
   
 @app.route('/translate', methods=['POST'])
 def translate_all_default():
+  # Get JSON data from request
   json_data = request.get_json()
-
+  # Check if 'text' field is within JSON data.
   if 'text' not in json_data:
     return jsonify({'error': 'No \'text\' field in JSON!', 'mappings': lang_maps})
 
@@ -86,7 +89,6 @@ def translate_all_default():
 def mappings():
   return jsonify({'mappings': lang_maps})
 
-# asgi_app = WsgiToAsgi(app)
-if __name__ == '__main__':
+if __name__ == "__main__":
   app.run('0.0.0.0', debug=True)
   
---
+++ run.sh
@@ -0,0 +1,1 @@
+hypercorn main:asgi_app --bind 0.0.0.0:$1
